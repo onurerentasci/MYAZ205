3763fc7e0370a8759036b7ace3cd632cd0941b64
onurerentasci <onurerentasci@gmail.com>
2022-10-18 01:36:00 +0300

    Add project files.

---
+++ OOP.AddAClass/Employee.cs
@@ -0,0 +1,48 @@
+﻿namespace OOP.AddAClass
+{
+    class Employee
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            set
+            {
+                _firstName = value;
+            }
+            get
+            {
+                return _firstName;
+            }
+        }
+        public string LastName 
+        { 
+            //Expression-bodied property accessors
+            get => _lastName; 
+            set => _lastName = value; 
+        }
+        
+        public int Age { get; set; }
+        //Auto-implemented property
+
+        public Employee() // default olarak mevcut
+        {
+            System.Console.WriteLine("Yapılandırıcı method çalıştı...");
+        }
+
+        public Employee(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public override string? ToString()
+        {
+            return $"{FirstName,-20} {LastName,-15} {Age, 5}";
+            // - ifadeler sola, + ifadeler sağa yaslama
+            // içerdeki sayılar karakter sayısı kadar boşluk oluşturma
+        }
+    }
+}
---
+++ OOP.AddAClass/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace OOP.AddAClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Employee>()
+            {
+                new Employee(){ FirstName = "Ahmet", LastName = "Türkoğlu", Age = 21},
+                new Employee("Mehmet", "Üstün", 15),
+                new Employee("Merve", "Aslan", 42)
+        };
+
+            // Console.WriteLine($"{employee2.FirstName} {employee2.LastName} {employee2.Age}");
+            // Console.WriteLine(employee3);
+            //list.Add(employee);
+            //list.Add(employee2);
+            //list.Add(employee3);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
